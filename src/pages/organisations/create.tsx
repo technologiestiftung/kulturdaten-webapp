13e057097956667e6506b493389d3b3cb758ddc1
Urs Schmidt <urs.schmidt@ts.berlin>
2023-05-31 10:42:47 +0200

    🏣 Setup createOrganisation page

---
+++ src/pages/organisations/create.tsx
@@ -0,0 +1,47 @@
+import React, { useState, FC } from 'react';
+import { CreateOrganization, OrganizationsService } from '../../generated-api-client';
+import { Input } from '../../components/InputField';
+import { Button } from '../../components/Button';
+import PageWrapper from '../../components/PageWrapper';
+
+const CreateOrganisation: FC = () => {
+	const [organisationName, organisationNameSet] = useState<string>('');
+	const [organisationDescription, organisationDescriptionSet] = useState<string>('');
+	const createOrganisationHandler = (e: Event) => {
+		e.preventDefault();
+		console.log('CREATE ORGANISATION');
+		const newOrganisationObject: CreateOrganization = {
+			name: { de: organisationName },
+			description: { de: organisationDescription },
+		};
+		OrganizationsService.postOrganizations(newOrganisationObject).then((res) => {
+			console.log('ORGANISATION', res);
+		});
+	};
+	return (
+		<PageWrapper>
+			<h1>Lege einen enue Organisation an</h1>
+			<form onSubmit={(e) => createOrganisationHandler(e)}>
+				<Input
+					type="text"
+					id="organisationName"
+					label={'Name'}
+					required
+					placeholder={'Hier bitte Name eingeben … '}
+					onChange={(value) => organisationNameSet(value)}
+				/>
+				<Input
+					type="text"
+					id="organisationDescription"
+					label={'Beschreibung'}
+					required
+					placeholder={'Hier bitte Beschreeibung eingeben … '}
+					onChange={(value) => organisationDescriptionSet(value)}
+				/>
+				<Button type="submit" label="Anlegen" />
+			</form>
+		</PageWrapper>
+	);
+};
+
+export default CreateOrganisation;
