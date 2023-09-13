ef4b593671bef3a26e1af8ac165a040f33e005b1
Sebastian Herrmann <sebastian.herrmann@ts.berlin>
2023-09-13 15:31:25 +0200

    fix: fix password validation error message

--- src/utils/validation.tsx
+++ src/utils/validation.tsx
@@ -10,7 +10,8 @@ export const validateEmail = (email: string): string => {
 };
 
 export const validatePassword = (password: string): string => {
-	return password.length >= 12 ? "" : "Bitte gib ein Passwort mit mindestens 8 Zeichen ein";
+	const minimumLength = 12;
+	return password.length >= minimumLength ? "" : `Bitte gib ein Passwort mit mindestens ${minimumLength} Zeichen ein`;
 };
 
 export const validateRepeatPassword = (password: string, passwordRepeat: string): string => {
