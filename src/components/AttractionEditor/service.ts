70cee6386bdb77ac4deeabeae87d965b181f685b
Sebastian Herrmann <sebastian.herrmann@ts.berlin>
2023-10-02 10:05:47 +0200

    fix: only use needed fields for attraction requests to fix updating

--- src/components/AttractionEditor/service.ts
+++ src/components/AttractionEditor/service.ts
@@ -1,11 +1,11 @@
 import { AdminAttraction } from "@api/client/models/AdminAttraction";
 import { CreateAttractionRequest } from "@api/client/models/CreateAttractionRequest";
 
-function createLanguagesObject(languages: Array<string>) {
+function createLanguagesObject(languages: Array<string>, existingValues: Record<string, string> | undefined) {
 	return languages.reduce(
-		(acc, lang) => {
-			acc[lang] = "";
-			return acc;
+		(result, lang) => {
+			result[lang] = existingValues?.[lang] ?? "";
+			return result;
 		},
 		{} as Record<string, string>,
 	);
@@ -17,13 +17,13 @@ export function getInitialRequest(
 ): CreateAttractionRequest {
 	return {
 		type: "type.Attraction",
-		title: createLanguagesObject(languages),
-		displayName: createLanguagesObject(languages),
-		description: createLanguagesObject(languages),
-		pleaseNote: createLanguagesObject(languages),
-		website: "",
+		title: createLanguagesObject(languages, attraction?.title),
+		displayName: createLanguagesObject(languages, attraction?.displayName),
+		description: createLanguagesObject(languages, attraction?.description),
+		pleaseNote: createLanguagesObject(languages, attraction?.pleaseNote),
+		website: attraction?.website ?? "",
 		inLanguages: languages,
-		tags: [],
-		...attraction,
+		tags: attraction?.tags || [],
+		externalLinks: attraction?.externalLinks || [],
 	};
 }
