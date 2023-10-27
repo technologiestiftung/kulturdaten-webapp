ad929962b816e64cc3062292c7e7124c002a9b5b
Sebastian Herrmann <sebastian.herrmann@ts.berlin>
2023-10-27 14:14:24 +0200

    refactor: support multiple organizations with memberships etc.

--- src/contexts/userContext.tsx
+++ src/contexts/userContext.tsx
@@ -1,19 +1,26 @@
 import { LoginResponse } from "@api/client/models/LoginResponse";
-import { createContext, FC, ReactNode, useEffect, useMemo, useState } from "react";
+import { Organization } from "@api/client/models/Organization";
+import { clearAccessToken, getAccessToken, storeAccessToken } from "@utils/auth";
+import { FC, ReactNode, createContext, useEffect, useMemo, useState } from "react";
 
-type Auth = Required<Required<LoginResponse>["data"]>;
-type User = Auth["user"];
+type LoginData = Required<LoginResponse>["data"];
+
+const STORAGE_KEY = "login-response";
 
 type UserContextType = {
-	userObject: User | null;
-	saveAuthObject: (authObject: LoginResponse["data"]) => void;
-	clearUser: () => void;
+	loginData: LoginData | null;
+	activeOrganization: Organization | null;
+	storeLoginResponse: (loginResponse: LoginResponse) => void;
+	selectOrganization: (organization: Organization | null) => void;
+	clearLoginData: () => void;
 };
 
 export const UserContext = createContext<UserContextType>({
-	userObject: null,
-	saveAuthObject: () => {},
-	clearUser: () => {},
+	loginData: null,
+	activeOrganization: null,
+	storeLoginResponse: () => {},
+	selectOrganization: () => {},
+	clearLoginData: () => {},
 });
 
 type UserContextProviderProps = {
@@ -21,29 +28,75 @@ type UserContextProviderProps = {
 };
 
 export const UserContextProvider: FC<UserContextProviderProps> = ({ children }: UserContextProviderProps) => {
-	const [userObject, userObjectSet] = useState<User | null>(null);
+	const [loginData, setLoginData] = useState<LoginData | null>(null);
+	const [activeOrganization, setActiveOrganization] = useState<Organization | null>(null);
 
-	const userContextValue = useMemo(() => {
-		const saveAuthObject = (loginResponse: LoginResponse["data"]) => {
-			if (loginResponse?.user) {
-				localStorage.setItem("userObject", JSON.stringify(loginResponse.user));
-				userObjectSet(loginResponse.user || null);
+	const initializeOrganizations = (loginData: LoginData | null) => {
+		const initialOrganization = loginData?.organizations?.[0] || null;
+		setActiveOrganization(initialOrganization);
+		if (initialOrganization) {
+			const matchingToken = loginData!.accessTokens.find(
+				(token) => token.organizationID === initialOrganization.identifier,
+			);
+			storeAccessToken(matchingToken!.token);
+		}
+	};
+
+	const restoreOrganizations = (loginData: LoginData) => {
+		const existingAccessToken = getAccessToken();
+		if (existingAccessToken) {
+			const matchingAccessToken = loginData.accessTokens.find(
+				(accessToken) => accessToken.token === existingAccessToken,
+			);
+			const organizationID = matchingAccessToken!.organizationID;
+			const selectedOrganization = loginData.organizations!.find(
+				(organization) => organization.identifier === organizationID,
+			)!;
+			setActiveOrganization(selectedOrganization);
+		}
+	};
+
+	const value = useMemo<UserContextType>(() => {
+		const storeLoginResponse = (loginResponse: LoginResponse) => {
+			const loginData = loginResponse.data!;
+			localStorage.setItem(STORAGE_KEY, JSON.stringify(loginData));
+			setLoginData(loginData);
+			initializeOrganizations(loginData);
+		};
+
+		const clearLoginData = () => {
+			localStorage.removeItem(STORAGE_KEY);
+			setLoginData(null);
+			setActiveOrganization(null);
+		};
+
+		const selectOrganization = (organization: Organization | null) => {
+			setActiveOrganization(organization);
+			if (organization) {
+				const matchingToken = loginData!.accessTokens.find((token) => token.organizationID === organization.identifier);
+				storeAccessToken(matchingToken!.token);
+			} else {
+				clearAccessToken();
 			}
 		};
 
-		const clearUser = () => {
-			localStorage.removeItem("userObject");
-			userObjectSet(null);
+		return {
+			loginData,
+			activeOrganization,
+			selectOrganization,
+			storeLoginResponse,
+			clearLoginData,
 		};
-		return { saveAuthObject, userObject, clearUser };
-	}, [userObject]);
+	}, [activeOrganization, loginData]);
 
 	useEffect(() => {
-		const user = localStorage.getItem("userObject");
-		if (user) {
-			userObjectSet(JSON.parse(user));
+		const loginDataString = localStorage.getItem(STORAGE_KEY);
+		const loginData = loginDataString ? (JSON.parse(loginDataString) as LoginData) : null;
+		setLoginData(loginData);
+		if (loginData) {
+			restoreOrganizations(loginData);
 		}
 	}, []);
 
-	return <UserContext.Provider value={userContextValue}>{children}</UserContext.Provider>;
+	return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,32 +1,28 @@
 import apiClient from "@api/client";
-import { clearAccessToken, storeAccessToken } from "@utils/auth";
 import { useRouter } from "next/router";
 import { useContext } from "react";
 import { UserContext } from "../contexts/userContext";
 
 export default function useUser() {
 	const router = useRouter();
-	const { userObject, clearUser, saveAuthObject } = useContext(UserContext);
+	const { loginData, activeOrganization, storeLoginResponse, clearLoginData } = useContext(UserContext);
 	const logOut = async () => {
-		clearAccessToken();
 		await router.push("/login");
-		clearUser();
+		clearLoginData();
 	};
 	const logIn = async (email: string, password: string) => {
 		const loginResponse = await apiClient.authentication.postAuthenticationLogin({
 			email: email.toLowerCase(),
 			password,
 		});
-		const loginResponseData = loginResponse.data;
-		if (loginResponseData?.accessToken) {
-			storeAccessToken(loginResponseData.accessToken);
-			saveAuthObject(loginResponseData);
-			router.push("/");
-		}
+		storeLoginResponse(loginResponse);
+		router.push("/");
 	};
 	return {
 		logIn,
 		logOut,
-		user: userObject,
+		user: loginData?.user || null,
+		organizations: loginData?.organizations || [],
+		activeOrganization,
 	};
 }
