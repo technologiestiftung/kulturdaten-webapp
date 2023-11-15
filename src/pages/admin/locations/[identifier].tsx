2e2c369160635710f6d2f230e5225974bc49f55b
Thorsten Diekhof <thorsten.diekhof@ts.berlin>
2023-11-15 15:32:09 +0100

    refactor: use withApiClientAndPagination

--- src/pages/admin/locations/[identifier].tsx
+++ src/pages/admin/locations/[identifier].tsx
@@ -1,27 +1,24 @@
-import { createAuthorizedClient } from "@api/client";
+import { withApiClientAndPagination } from "@utils/data";
 import { Location } from "@api/client/models/Location";
-import LocationDetailsPage from "@components/LocationDetailsPage";
-import { getAccessTokenFromContext } from "@utils/auth";
-import { loadMessages } from "@utils/i18n";
-import withAuth from "@utils/withAuth";
 import { GetServerSideProps } from "next";
+import LocationDetailsPage from "@/src/components/LocationDetailsPage";
+import withAuth from "@/src/utils/withAuth";
 
 interface Props {
 	location: Location;
 }
 
-export const getServerSideProps: GetServerSideProps<Props> = async (context) => {
-	const accessToken = getAccessTokenFromContext(context);
-	const apiClient = createAuthorizedClient(accessToken);
-	const identifier = context.query.identifier as string;
-	const response = await apiClient.discoverCulturalData.getLocations1(identifier);
-	const location = response.data!.location!;
-	return {
-		props: {
-			location,
-			messages: await loadMessages(context.locale!),
-		},
-	};
-};
+export const getServerSideProps: GetServerSideProps<Props> = (context) =>
+	withApiClientAndPagination<Props>(context)(async ({ apiClient, messages }) => {
+		const identifier = context.query.identifier as string;
+		const response = await apiClient.discoverCulturalData.getLocations1(identifier);
+		const location = response.data!.location!;
+		return {
+			props: {
+				location,
+				messages,
+			},
+		};
+	});
 
 export default withAuth(LocationDetailsPage);
