ba15bfc7d81d7219c1c2de844824ca5bbfb71f7a
Sebastian Herrmann <sebastian.herrmann@ts.berlin>
2023-11-16 14:58:30 +0100

    refactor: extract LoginPage into new component

---
+++ src/components/LoginPage/index.tsx
@@ -0,0 +1,91 @@
+import { ApiError } from "@api/client/core/ApiError";
+import Button from "@components/Button";
+import FormWrapper from "@components/FormWrapper";
+import { Input } from "@components/InputField";
+import Page from "@components/Page";
+import useUser from "@hooks/useUser";
+import { validateEmail } from "@utils/validation";
+import React, { useState } from "react";
+import { useTranslations } from "use-intl";
+
+interface ErrorMessages {
+	general: string | undefined;
+	email: string | undefined;
+}
+
+const initialErrorMessages: ErrorMessages = {
+	general: undefined,
+	email: undefined,
+};
+
+export default function LoginPage() {
+	const t = useTranslations("Login");
+	const [email, emailSet] = useState<string>("");
+	const [password, passwordSet] = useState<string>("");
+	const [errorMessages, errorMessagesSet] = useState<ErrorMessages>(initialErrorMessages);
+	const [emailPristine, emailPristineSet] = useState<boolean>(true);
+	const { logIn } = useUser();
+
+	const onEmailChange = (value: string) => {
+		const emailValid = validateEmail(value);
+		errorMessagesSet({ ...errorMessages, email: emailValid });
+		emailSet(value);
+	};
+
+	const onPasswordChange = (value: string) => {
+		errorMessagesSet(initialErrorMessages);
+		passwordSet(value);
+	};
+
+	const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const emailValid = validateEmail(email);
+		errorMessagesSet({ ...initialErrorMessages, email: emailValid ? emailValid : undefined });
+		try {
+			await logIn(email, password);
+		} catch (error) {
+			const apiError = error as ApiError;
+			if (apiError?.status) {
+				errorMessagesSet({
+					...errorMessages,
+					general: `Login fehlgeschlagen, ${apiError.status}`,
+				});
+			} else {
+				errorMessagesSet({ ...errorMessages, general: "Verbindung fehlgeschlagen" });
+			}
+		}
+	};
+
+	return (
+		<Page metadata={{ title: t("page-title") }} showNavigation={false}>
+			<FormWrapper>
+				<h1>Bei kulturdaten.berlin einloggen</h1>
+				<p className="mt-2 mb-8">
+					kulturdaten.berlin ist kostenlos - und macht deine Programminfos einfacher zugänglich!
+				</p>
+				<form onSubmit={handleLogin}>
+					<Input
+						type="email"
+						id="email"
+						label={"Email"}
+						required
+						placeholder={"Hier bitte Email eingeben … "}
+						errorMessage={emailPristine ? undefined : errorMessages.email}
+						onChange={(value) => onEmailChange(value)}
+						setPristine={emailPristineSet}
+					/>
+					<Input
+						type="password"
+						id="password"
+						label={"Password"}
+						required
+						placeholder={"Hier bitte Passwort eingeben … "}
+						onChange={(value) => onPasswordChange(value)}
+					/>
+					<Button type="submit">Login</Button>
+				</form>
+				{errorMessages.general && <p aria-live="assertive">{errorMessages.general}</p>}
+			</FormWrapper>
+		</Page>
+	);
+}
