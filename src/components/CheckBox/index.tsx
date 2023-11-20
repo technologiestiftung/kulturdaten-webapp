fddbb8370922409b37a2568b67027c29b6d53f2c
Thorsten Diekhof <thorsten.diekhof@ts.berlin>
2023-11-20 13:09:46 +0100

    feat: checkbox added for virtual location If larger areas are desired, as in the screenshot, this should be covered in a separate story.

---
+++ src/components/CheckBox/index.tsx
@@ -0,0 +1,41 @@
+import { borderRadiuses, colors } from "@common/styleVariables";
+import styled, { CSSObject } from "@emotion/styled";
+import { DetailedHTMLProps, InputHTMLAttributes } from "react";
+
+const errorStyle: CSSObject = {
+	borderColor: colors.error,
+};
+
+const StyledCheckbox = styled.input<Props>(({ error, disabled, readOnly }) => ({
+	appearance: "none",
+	margin: 0,
+	marginLeft: "0.5em",
+	verticalAlign: "middle",
+	width: "1.2em",
+	height: "1.2em",
+	border: `1px solid ${colors.mediumContrast}`,
+	borderRadius: borderRadiuses.small,
+	backgroundColor: colors.white,
+	cursor: "pointer",
+	"&:checked": {
+		backgroundColor: colors.blueDark,
+	},
+	"&:hover": !disabled &&
+		!readOnly && {
+			boxShadow: `0px 0px 0px 2px ${colors.neutral300}`,
+		},
+	"&:disabled": {
+		backgroundColor: colors.neutral200,
+		cursor: "not-allowed",
+	},
+	...(error ? errorStyle : {}),
+}));
+
+export interface Props extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
+	error?: string | null;
+}
+
+export default function Checkbox(props: Props) {
+	const { type = "checkbox", ...otherProps } = props;
+	return <StyledCheckbox type={type} {...otherProps} />;
+}
--- src/components/LocationEditor/index.tsx
+++ src/components/LocationEditor/index.tsx
@@ -10,6 +10,7 @@ import Textarea from "../Textarea";
 import Buttons from "./Buttons";
 import { getInitialRequest } from "./service";
 import AddressFormFields from "../AddressFormFields";
+import Checkbox from "../CheckBox";
 
 interface Props {
 	location: Location | null;
@@ -97,48 +98,67 @@ export default function LocationEditor(props: Props) {
 				required={false}
 			/>
 			<Spacer size={15} />
-			<AddressFormFields
-				streetAddress={locationRequest.address?.streetAddress}
-				addressLocality={locationRequest.address?.addressLocality}
-				postalCode={locationRequest.address?.postalCode}
-				description={locationRequest.address?.description}
-				onStreetAddressChange={(event) => {
+			<FormField
+				component={Checkbox}
+				type="checkbox"
+				label={t("label-isVirtual")}
+				id="isVirtual"
+				checked={locationRequest.isVirtual}
+				onChange={(event) => {
 					setLocationRequest((prev) => ({
 						...prev,
-						address: {
-							...prev.address,
-							streetAddress: event.target.value,
-						},
-					}));
-				}}
-				onAddressLocalityChange={(event) => {
-					setLocationRequest((prev) => ({
-						...prev,
-						address: {
-							...prev.address,
-							addressLocality: event.target.value,
-						},
-					}));
-				}}
-				onPostalCodeChange={(event) => {
-					setLocationRequest((prev) => ({
-						...prev,
-						address: {
-							...prev.address,
-							postalCode: event.target.value,
-						},
-					}));
-				}}
-				onDescriptionChange={(event) => {
-					setLocationRequest((prev) => ({
-						...prev,
-						address: {
-							...prev.address,
-							description: event.target.value,
-						},
+						isVirtual: event.target.checked,
 					}));
 				}}
 			/>
+
+			{!locationRequest.isVirtual && (
+				<>
+					<Spacer size={15} />
+					<AddressFormFields
+						streetAddress={locationRequest.address?.streetAddress}
+						addressLocality={locationRequest.address?.addressLocality}
+						postalCode={locationRequest.address?.postalCode}
+						description={locationRequest.address?.description}
+						onStreetAddressChange={(event) => {
+							setLocationRequest((prev) => ({
+								...prev,
+								address: {
+									...prev.address,
+									streetAddress: event.target.value,
+								},
+							}));
+						}}
+						onAddressLocalityChange={(event) => {
+							setLocationRequest((prev) => ({
+								...prev,
+								address: {
+									...prev.address,
+									addressLocality: event.target.value,
+								},
+							}));
+						}}
+						onPostalCodeChange={(event) => {
+							setLocationRequest((prev) => ({
+								...prev,
+								address: {
+									...prev.address,
+									postalCode: event.target.value,
+								},
+							}));
+						}}
+						onDescriptionChange={(event) => {
+							setLocationRequest((prev) => ({
+								...prev,
+								address: {
+									...prev.address,
+									description: event.target.value,
+								},
+							}));
+						}}
+					/>
+				</>
+			)}
 			<Spacer size={20} />
 			<Buttons location={location} onUpdated={handleUpdatedStatus} submitLabel={submitLabel} />
 		</form>
