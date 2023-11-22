531c1e930088a42fa51f46de0bf801dbe9386a93
Sebastian Herrmann <sebastian.herrmann@ts.berlin>
2023-11-22 10:16:05 +0100

    fix: pre-sort tags before nesting, so that the order doesn’t matter

--- src/utils/tags.ts
+++ src/utils/tags.ts
@@ -5,20 +5,32 @@ export interface TagGroup extends Tag {
 }
 
 export function groupTags(tags: Tag[]): TagGroup[] {
-	return tags.reduce((tagGroups: TagGroup[], tag: Tag) => {
-		if (isGroup(tag)) {
-			const newGroup: TagGroup = {
-				...tag,
-				children: [],
-			};
-			tagGroups.push(newGroup);
-			return tagGroups;
-		}
-		// We assume that groups always come before their children.
-		const matchingGroup = tagGroups.find((group) => getGroupIdentifier(group) === getGroupIdentifier(tag))!;
-		matchingGroup.children.push(tag);
-		return tagGroups;
-	}, []);
+	return (
+		tags
+			// Sort tags so that all groups are before the children.
+			.sort((tagA, TagB) => {
+				if (isGroup(tagA)) {
+					return -1;
+				}
+				if (isGroup(TagB)) {
+					return 1;
+				}
+				return 0;
+			})
+			.reduce((tagGroups: TagGroup[], tag: Tag) => {
+				if (isGroup(tag)) {
+					const newGroup: TagGroup = {
+						...tag,
+						children: [],
+					};
+					tagGroups.push(newGroup);
+					return tagGroups;
+				}
+				const matchingGroup = tagGroups.find((group) => getGroupIdentifier(group) === getGroupIdentifier(tag))!;
+				matchingGroup.children.push(tag);
+				return tagGroups;
+			}, [])
+	);
 }
 
 const getGroupIdentifier = (tag: Tag) => tag.identifier.split(".").slice(0, 3).join(".");
