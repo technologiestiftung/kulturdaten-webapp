b5a2a9570ab1b2e9e89299efc46886c77ebb23c3
Sebastian Herrmann <sebastian.herrmann@ts.berlin>
2023-11-23 16:22:51 +0100

    refactor: remove groups from navigation

--- src/components/Page/Navigation/index.tsx
+++ src/components/Page/Navigation/index.tsx
@@ -1,5 +1,5 @@
 import ROUTES from "@common/routes";
-import { colors, fontSizes, fontWeights, spacings, widths } from "@common/styleVariables";
+import { colors, fontWeights, spacings, widths } from "@common/styleVariables";
 import { IconName } from "@components/Icon";
 import Spacer from "@components/Spacer";
 import styled from "@emotion/styled";
@@ -10,37 +10,19 @@ import { useTranslations } from "use-intl";
 import NavigationLink, { NavigationButton } from "./NavigationLink";
 import OrganizationSelect from "./OrganizationSelect";
 
-type NavigationGroupType = "user" | "admin";
-
-type NavigationGroup = {
-	type: NavigationGroupType;
-	i18nKey: keyof IntlMessages["Navigation"];
-	links: Array<Link>;
-};
-
 type Link = {
 	href: string;
 	icon: IconName;
 	i18nKey: keyof IntlMessages["Navigation"];
 };
 
-function getNavigationGroups(isAdmin: boolean): Array<NavigationGroup> {
-	const userNavigationGroup: NavigationGroup = {
-		type: "user",
-		i18nKey: "group-user",
-		links: [{ href: ROUTES.user.attractions(), i18nKey: "link-user-attractions", icon: "star" }],
-	};
-	const adminNavigationGroup: NavigationGroup = {
-		type: "admin",
-		i18nKey: "group-admin",
-		links: [
-			{ href: ROUTES.admin.attractions(), i18nKey: "link-admin-attractions", icon: "star" },
-			{ href: ROUTES.admin.locations(), i18nKey: "link-admin-locations", icon: "map-pin" },
-			{ href: ROUTES.admin.organizations(), i18nKey: "link-admin-organizations", icon: "drama" },
-			{ href: ROUTES.admin.users(), i18nKey: "link-admin-users", icon: "users" },
-		],
-	};
-	return [userNavigationGroup, ...(isAdmin ? [adminNavigationGroup] : [])];
+function getLinks(): Array<Link> {
+	return [
+		{ href: ROUTES.admin.attractions(), i18nKey: "link-admin-attractions", icon: "star" },
+		{ href: ROUTES.admin.locations(), i18nKey: "link-admin-locations", icon: "map-pin" },
+		{ href: ROUTES.admin.organizations(), i18nKey: "link-admin-organizations", icon: "drama" },
+		{ href: ROUTES.admin.users(), i18nKey: "link-admin-users", icon: "users" },
+	];
 }
 
 const Container = styled.div({
@@ -61,28 +43,18 @@ const PageTitle = styled.div({
 	fontWeight: fontWeights.medium,
 });
 
-const Group = styled.nav({});
-
-const GroupTitle = styled.div({
-	color: colors.black,
-	fontWeight: fontWeights.medium,
-	fontSize: fontSizes.small,
-});
-
-const GroupList = styled.ul({
+const LinkList = styled.ul({
 	listStyle: "none",
 	padding: 0,
-	margin: `0px -${spacings.get(2)}`,
 });
 
-const GroupItem = styled.li({});
+const LinkItem = styled.li({});
 
 export default function Navigation() {
 	const t = useTranslations("Navigation");
-	const { organizations, activeOrganization, activeRole, selectOrganization, logOut } = useUser();
+	const { organizations, activeOrganization, selectOrganization, logOut } = useUser();
 	const router = useRouter();
-	const isAdmin = activeRole === "admin";
-	const navigationGroups = useMemo(() => getNavigationGroups(isAdmin), [isAdmin]);
+	const links = useMemo(() => getLinks(), []);
 	const isLinkActive = useCallback(
 		(href: string) => {
 			const route = router.route;
@@ -96,7 +68,7 @@ export default function Navigation() {
 	);
 	return (
 		<Container>
-			<div>
+			<nav>
 				<PageTitle>{t("title")}</PageTitle>
 				<Spacer size={20} />
 				<OrganizationSelect
@@ -105,31 +77,25 @@ export default function Navigation() {
 					onSelectOrganization={selectOrganization}
 				/>
 				<Spacer size={20} />
-				{navigationGroups.map((group, index) => (
-					<Group key={index}>
-						<GroupTitle>{t(group.i18nKey)}</GroupTitle>
-						<GroupList>
-							{group.links.map((link, index) => (
-								<GroupItem key={index}>
-									<NavigationLink href={link.href} icon={link.icon} isActive={isLinkActive(link.href)}>
-										{t(link.i18nKey)}
-									</NavigationLink>
-								</GroupItem>
-							))}
-						</GroupList>
-						<Spacer size={20} />
-					</Group>
-				))}
-			</div>
-			<div>
-				<GroupList>
-					<GroupItem>
+				<LinkList>
+					{links.map((link, index) => (
+						<LinkItem key={index}>
+							<NavigationLink href={link.href} icon={link.icon} isActive={isLinkActive(link.href)}>
+								{t(link.i18nKey)}
+							</NavigationLink>
+						</LinkItem>
+					))}
+				</LinkList>
+			</nav>
+			<nav>
+				<LinkList>
+					<LinkItem>
 						<NavigationButton onClick={logOut} icon="log-out">
 							{t("link-logout")}
 						</NavigationButton>
-					</GroupItem>
-				</GroupList>
-			</div>
+					</LinkItem>
+				</LinkList>
+			</nav>
 		</Container>
 	);
 }
