67015e98bcb9978330549c4db7059283f6e0b663
Sebastian Herrmann <sebastian.herrmann@ts.berlin>
2023-12-05 11:26:33 +0100

    feat: improve responsiveness of PageTitleHeader

--- src/components/PageTitle/index.tsx
+++ src/components/PageTitle/index.tsx
@@ -1,10 +1,13 @@
-import { colors, fontSizes, fontWeights } from "@common/styleVariables";
+import { colors, fontSizes, fontWeights, mediaQueries } from "@common/styleVariables";
 import styled from "@emotion/styled";
 
 const PageTitle = styled.h1({
-	fontSize: fontSizes.extraLarge,
+	fontSize: fontSizes.large,
 	fontWeight: fontWeights.default,
 	color: colors.blueDark,
+	[mediaQueries.m]: {
+		fontSize: fontSizes.extraLarge,
+	},
 });
 
 export default PageTitle;
