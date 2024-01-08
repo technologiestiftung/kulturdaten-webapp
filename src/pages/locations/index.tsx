dce30bc5e55665768ccd8dd44bdd0f4f223dd520
Thorsten Diekhof <thorsten.diekhof@ts.berlin>
2024-01-08 13:30:33 +0100

    feat: use the current way to fetch data

--- src/pages/locations/index.tsx
+++ src/pages/locations/index.tsx
@@ -1,12 +1,20 @@
 import LocationsPage from "@components/pages/LocationsPage";
-import { loadMessages } from "@services/i18n";
+import { getPaginationProps } from "@services/pagination";
+import withApiClientAndPagination from "@services/withApiClientAndPagination";
 import withAuth from "@services/withAuth";
-import { GetStaticProps } from "next";
+import { ComponentProps } from "react";
 
-export const getStaticProps: GetStaticProps = async (context) => ({
-	props: {
-		messages: await loadMessages(context.locale!),
-	},
+type Props = ComponentProps<typeof LocationsPage>;
+
+export const getServerSideProps = withApiClientAndPagination<Props>(async ({ apiClient, page, pageSize }) => {
+	const response = await apiClient.discoverCulturalData.getLocations(page, pageSize);
+	const data = response.data!;
+	return {
+		props: {
+			locations: data.locations || [],
+			pagination: getPaginationProps(data),
+		},
+	};
 });
 
 export default withAuth(LocationsPage);
